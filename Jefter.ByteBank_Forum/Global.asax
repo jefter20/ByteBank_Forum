95351f13fc9d4da05f43b5298fecc2ecf197fd18
Jefter Alves <jefteralveskf2@gmail.com>
2021-03-16 17:06:58 -0300

    Primeiro commit - projeto final do Curso de ASP.NET Identity parte 1 - Alura

---
+++ Jefter.ByteBank_Forum/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jefter.ByteBank_Forum.MvcApplication" Language="C#" %>
---
+++ Jefter.ByteBank_Forum/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Jefter.ByteBank_Forum.App_Start.Identity;
+using Jefter.ByteBank_Forum.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(Jefter.ByteBank_Forum.Startup))]
+
+namespace Jefter.ByteBank_Forum
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.CreatePerOwinContext<DbContext>(() =>
+                new IdentityDbContext<UsuarioAplicacao>("DefaultConnection"));
+
+            builder.CreatePerOwinContext<IUserStore<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var dbContext = contextoOwin.Get<DbContext>();
+                    return new UserStore<UsuarioAplicacao>(dbContext);
+                });
+
+            builder.CreatePerOwinContext<UserManager<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var userStore = contextoOwin.Get<IUserStore<UsuarioAplicacao>>();
+                    var userManager = new UserManager<UsuarioAplicacao>(userStore);
+
+                    var userValidator = new UserValidator<UsuarioAplicacao>(userManager);
+                    userValidator.RequireUniqueEmail = true;
+
+                    userManager.UserValidator = userValidator;
+                    userManager.PasswordValidator = new SenhaValidador()
+                    {
+                        TamanhoRequerido = 6,
+                        ObrigatorioCaracteresEspeciais = true,
+                        ObrigatorioDigitos = true,
+                        ObrigatorioLowerCase = true,
+                        ObrigatorioUpperCase = true
+
+                    };
+
+                    userManager.EmailService = new EmailServico();
+
+                    var dataProtectionProvider = opcoes.DataProtectionProvider;
+                    var dataProtectionProviderCreated = dataProtectionProvider.Create("ByteBank.Forum");
+
+                    userManager.UserTokenProvider = new DataProtectorTokenProvider<UsuarioAplicacao>(dataProtectionProviderCreated);
+
+                    return userManager;
+                });
+        }
+    }
+
+}
